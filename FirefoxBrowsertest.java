00105c4a3ccc28c713def8b242b3b9f80d1b4493
Abhinaw-2612 <abhi.naw1226@gmail.com>
2026-06-08 16:30:14 +0530

    Add files via upload

---
+++ FirefoxBrowsertest.java
@@ -0,0 +1,106 @@
+package com.seleniumAssOne;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirefoxBrowsertest {
+
+    @Test
+    public void  FirefoxTest1() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest2() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://demo.opencart.com/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest3() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.bing.com/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest4() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.chase.com/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void FirefoxTest5() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://altoro.testfire.net/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest6() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest7() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }
+
+    @Test
+    public void  FirefoxTest8() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://altoro.testfire.net/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        
+    }
+    
+    @Test
+    public void FirefoxTest9() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.demoblaze.com/index.html");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        System.out.println(title);
+        String URL = driver.getCurrentUrl();
+        System.out.println(URL);
+    }}
---
+++ LaunchDifferentBrowsers.java
@@ -0,0 +1,34 @@
+package com.TestNGDemoTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LaunchDifferentBrowsers {
+	
+	@Test
+	public void ChromeBrowserTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/downloads/");// Links
+		// driver.manage().window().maximize();//maximize to full screen
+		// Thread.sleep(3000);
+		driver.manage().window().maximize();// minimizes on screen
+
+	}
+
+	public void FirefoxBrowserTest() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.selenium.dev/downloads/");
+		driver.manage().window().maximize();// minimizes on screen
+
+	}
+
+	public void EdgeBrowserTest() {
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.selenium.dev/downloads/");
+		driver.manage().window().maximize();// minimizes on screen
+
+	}
+}
